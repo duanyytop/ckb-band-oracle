b742c720ecc3269b29bee8d07dd295d2cbe2c0c7
duanyytop <duanyytop@gmail.com>
2020-10-07 17:12:33 +0800

    chore: Update ckb node url

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
-const CKB_NODE_URL = 'https://prototype.ckbapp.dev/testnet/rpc'
-const CKB_INDEXER_URL = 'https://prototype.ckbapp.dev/testnet/indexer'
-const CKB_WS_URL = 'wss://ws-prototype.ckbapp.dev/testnet/rpc'
+const CKB_NODE_URL = 'https://testnet.ckb.dev/rpc'
+const CKB_INDEXER_URL = 'https://testnet.ckb.dev/indexer_rpc'
+const CKB_WS_URL = 'https://ws-ckbrpc.ckbapp.dev/testnet/rpc'
 const PRI_KEY = process.env.PRIVATE_KEY
 
 const BAND_SYMBOL = '62616e64' // Hex of "band"
