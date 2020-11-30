e66b9c8db254f113e3b6dd974d6aad7001556383
duanyytop <duanyytop@gmail.com>
2020-11-30 21:17:08 +0800

    chore: Update transaction fee

--- src/poster/rpc.js
+++ src/poster/rpc.js
@@ -7,8 +7,8 @@ const { remove0x, generateBandData, parseBandData } = require('../utils/utils')
 const ckb = new CKB(CKB_NODE_URL)
 const PUB_KEY = ckb.utils.privateKeyToPublicKey(PRI_KEY)
 const ARGS = '0x' + ckb.utils.blake160(PUB_KEY, 'hex')
-const FEE = new BN(5000)
-const EACH_CAPACITY = new BN(40000000000)
+const FEE = new BN(25000)
+const EACH_CAPACITY = new BN(500000000000)
 
 const secp256k1LockScript = async () => {
   const secp256k1Dep = (await ckb.loadDeps()).secp256k1Dep
